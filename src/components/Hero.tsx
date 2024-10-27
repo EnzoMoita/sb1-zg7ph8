738376f5ce0e3e703272344516d9e55cfcdae359
EnzoMoita <enzosadourado@hotmail.com>
2024-10-26 23:16:31 -0300

    Fix position for image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,8 +3,8 @@ import { ArrowRight } from 'lucide-react';
 export default function Hero() {
   return (
     <div className="relative bg-white overflow-hidden">
-      <div className="max-w-7xl mx-auto">
-        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32">
+      <div className="max-w-7xl mx-auto flex flex-col lg:flex-row">
+        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32 lg:w-1/2">
           <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 lg:mt-16 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
@@ -32,13 +32,13 @@ export default function Hero() {
             </div>
           </main>
         </div>
-      </div>
-      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-        <img
-          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src="https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1973&q=80"
-          alt="Modern apartment building"
-        />
+        <div className="lg:w-1/2">
+          <img
+            className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+            src="/City girl-pana.svg"
+            alt="Modern apartment building"
+          />
+        </div>
       </div>
     </div>
   );
